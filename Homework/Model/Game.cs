c6799dfac8678355634f12515ec34d024778db95
JTERMcode <jamarthompson954@gmail.com>
2024-02-22 13:51:58 -0700

    Add project files.

---
+++ Homework/Model/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework.Model
+{
+    internal class Games
+    {
+        public string Name { get; set; }
+
+        public int Price {  get; set; }
+
+        public void introduceself()
+        {
+            Console.WriteLine($"Welcome to {Name} this game cost {Price} dollars");
+        }
+
+
+    }
+}
---
+++ Homework/Program.cs
@@ -0,0 +1,39 @@
+﻿using Homework.Model;
+
+internal class Program
+{
+   
+    private static void Main(string[] args)
+    {
+        List<Games> list = new List<Games>();
+
+        Games FPS = new Games();
+        Games RPG = new Games();
+        Games MMORPG = new Games(); 
+
+        FPS.Name = "Valorant";
+        FPS.Price = 0;
+
+        RPG.Name = "Skyrim";
+        RPG.Price = 60;
+
+        MMORPG.Name = "Lost Ark";
+        MMORPG.Price = 60;
+
+        list.Add( FPS );
+        list.Add( RPG );
+        list.Add( MMORPG );
+
+        foreach (Games Game in list) 
+        {
+            Console.WriteLine(Game.Name);
+
+            Game.introduceself();   
+
+        }      
+
+    }
+}
+
+
+
